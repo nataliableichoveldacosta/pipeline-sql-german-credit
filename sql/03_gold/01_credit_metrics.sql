5ac7567336817be1d4245e02dca07a27d4407f8a
nataliableichoveldacosta <nataliableichoveldacosta@gmail.com>
2026-03-09 09:31:52 -0300

    Update 01_credit_metrics.sql

--- sql/03_gold/01_credit_metrics.sql
+++ sql/03_gold/01_credit_metrics.sql
@@ -1,1 +1,13 @@
+-- Credit metrics by purpose
 
+CREATE TABLE gold_credit_by_purpose AS
+
+SELECT
+    Purpose,
+    COUNT(*) AS total_loans,
+    AVG(Credit_amount) AS avg_credit_amount,
+    AVG(Duration) AS avg_duration
+
+FROM silver_german_credit
+
+GROUP BY Purpose;
